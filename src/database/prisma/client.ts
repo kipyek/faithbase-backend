15869a9bc409f219e4cc453eb37038ecd6296267
unknown <deniskipyegon88@gmail.com>
2026-05-18 17:24:27 +0300

    chore: fix ESM default import for PrismaClient

--- src/database/prisma/client.ts
+++ src/database/prisma/client.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+
+const { PrismaClient } = pkg;
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma: PrismaClient = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
